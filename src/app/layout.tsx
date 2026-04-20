71c733fa93d271572c6d88e487bc6597195ddd50
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-04-20 15:16:23 +0530

    fixed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Devium",
